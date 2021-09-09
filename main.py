79fabc7592ee37ea2c518eb3508cec2d1905d969
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2021-09-09 15:32:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from utils import *
 
 client = commands.Bot(command_prefix=".")
 
+@client.event
+async def on_ready():
+    print("online")
+
 @client.command()
 async def support(ctx):
     await ctx.author.send(f"Please describe your problem.\nOur Staff team will be with you as soon as possible.\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓")
@@ -26,11 +30,9 @@ async def on_message(message):
                 embed.add_field(name="**⚠ New Message ⚠**", value=f"**User: **{message.author.mention}\n**Message: **{message.content}\n**User-ID: ** {message.author.id}")
                 embed.set_footer(text="Use '.reply UserId' to send a message to the User")
                 msg1 = await client.get_guild(882320033840255026).get_channel(channel_exists.id).send(embed=embed)
-                await msg1.add_reaction("❌")
             else:
                new_channel = await client.get_guild(882320033840255026).create_text_channel(name=f"support-{message.author}", reason="New ModMail ticket created.")
                msg = await client.get_guild(882320033840255026).get_channel(new_channel.id).send(f"⚠️ **New Support Ticket** ⚠️\n**User:** {message.author.mention}\n**User-ID:** {message.author.id}\n\n**Message:** {message.content}")
-               await msg.add_reaction("❌") # wenn man auf die reaction drückt soll was passieren
 
 
             
@@ -43,6 +45,14 @@ async def reply(ctx, member : discord.User, *, args):
     await member.send(embed=embed)
 
 @client.command()
+async def close(ctx, member : discord.Member, *, reason=None):
+    c = ctx.channel
+    await c.delete(reason=reason)
+    await member.send(f"Your ticket has been closed. Reason: {reason}")
+
+
+@client.command()
 async def test(ctx):
     await ctx.send(str(ctx.author).lower().replace("#", "").replace(" ", "-").replace("|", "-"))
+
 client.run(token)
